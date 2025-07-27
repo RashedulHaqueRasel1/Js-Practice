0f3b0104d806284e36777825239b784953ec4581
RashedulHaqueRasel1 <rashedulhaquerasel1@gmail.com>
2025-07-27 23:39:40 +0600

    day4 practice

---
+++ day4.js
@@ -0,0 +1,120 @@
+
+
+// const name = "Rashedul Haque Rasel";
+
+// let reverse = ""
+
+// for (let i = name.length - 1; i >= 0; i--) {
+//     // console.log(name[i])
+//      reverse += name[i]
+// }
+
+// console.log(reverse)
+
+
+// const data = [
+//     { name: "Rasel", age: 21, passion: "Doctor" },
+//     { name: "Manik", age: 34, passion: "Developer" },
+//     { name: "Robiul", age: 76, passion: "Student" },
+//     { name: "Lemon", age: 8, passion: "Doctor" },
+// ]
+
+// for (let i = 0; i < data.length; i++) {
+//     // console.log(data[i])
+//     const filterData = data[i]
+
+//     if (filterData.passion === "Developer") {
+//         console.log(filterData)
+//     }
+// }
+
+
+// const numbers = [34, 64, 65, 87, 65, 76, 23, 434, 45]
+
+// let total = 0
+
+// function totalNumber(numbers) {
+//     // console.log(numbers)
+//     for (let i = 0; i < numbers.length; i++) {
+//         // console.log(numbers[i])
+//          total += numbers[i]
+
+//     }
+
+// }
+
+// totalNumber(numbers)
+
+// console.log(total)
+
+
+
+
+
+// const numbers = [2,3,2,4,5,6,1]
+
+// let total = 0
+
+// function totalNumber(numbers) {
+//     // console.log(numbers)
+
+//     for (let i = 0; i < numbers.length; i++) {
+//         //     // console.log(numbers[i])
+//         const number = numbers[i]
+
+//         if (number % 2 === 0) {
+//             total += number
+//         }
+
+
+
+//     }
+
+// }
+
+// totalNumber(numbers)
+
+// console.log(total)
+
+
+
+// const data = [
+//     { name: "Rasel", age: 21, passion: "Doctor" },
+//     { name: "Manik", age: 34, passion: "Developer" },
+//     { name: "Robiul", age: 16, passion: "Student" },
+//     { name: "Lemon", age: 8, passion: "Doctor" },
+// ]
+
+// // const result = data.filter((item)=> item.age > 20 )
+// function adults(data) {
+//     // console.log(data)
+
+//     for (let i = 0; i < data.length; i++) {
+//         // console.log(data[i])
+//         if (data[i].age > 20) {
+//             console.log(data[i])
+//         }
+//     }
+// }
+// adults(data)
+// console.log(result)
+
+
+
+// const words = "Rashedul Haque Rasel"
+// let vowel = "aeiouAEIOU"
+// let count = 0
+// for (let i = 0; i < words.length; i++) {
+//     // console.log(words[i])
+
+//     const word = words[i]
+//     // console.log(word)
+//     if (vowel.includes(word)) {
+//         count ++
+//     }
+
+// }
+
+// console.log(count)
+
+ 
